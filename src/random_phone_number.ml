158a5e088f004f36ff16c066e7275db9349f918a
Victor Schubert <v@schu.be>
2019-12-17 18:38:54 +0100

    generate really random phone numbers

--- src/random_phone_number.ml
+++ src/random_phone_number.ml
@@ -2,11 +2,13 @@ let choose ?(location = Location.France) () =
   match location with
   | France ->
       let digits =
-        Random.int 1000000 |> string_of_int |> Pad.left ~padWith:"0" 6
+        Js.Math.random_int 0 1000000
+        |> string_of_int |> Pad.left ~padWith:"0" 6
       in
       "+33782" ^ digits
   | Germany ->
       let digits =
-        Random.int 1000000000 |> string_of_int |> Pad.left ~padWith:"0" 7
+        Js.Math.random_int 0 1000000000
+        |> string_of_int |> Pad.left ~padWith:"0" 7
       in
       "+4930" ^ digits
