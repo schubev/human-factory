9397101d6b96a872a4f36518184ca00169afe3ac
Victor Schubert <v@schu.be>
2020-01-07 09:37:34 +0100

    use fictitious french phone numbers

--- src/random_phone_number.ml
+++ src/random_phone_number.ml
@@ -1,8 +1,21 @@
-let chooseMobileFrance () =
-  let digits =
+let randomSuffixFrance prefix =
+  let suffix =
     Js.Math.random_int 0 1000000 |> string_of_int |> Pad.left ~padWith:"0" 6
   in
-  "+33782" ^ digits
+  prefix ^ suffix
+
+let chooseMobileFrance () =
+  (* Check out docs/fictitious_phone_numbers.org. *)
+  randomSuffixFrance "+3363998"
+
+let chooseLandlineFrance () =
+  let prefixes =
+    (* Check out docs/fictitious_phone_numbers.org. *)
+    [|"+3319900"; "+3326191"; "+3335301"; "+3346571"; "+3353649"|]
+  in
+  let prefix_index = Js.Math.random_int 0 (Array.length prefixes) in
+  let prefix = prefixes.(prefix_index) in
+  randomSuffixFrance prefix
 
 let chooseLandlineGermany () =
   let digits =
